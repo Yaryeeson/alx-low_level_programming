5528a1a11b47933ba5fe9a107d204f2b58036cc0
Yaryeeson <caryeeson@gmail.com>
2023-04-20 21:15:20 -0700

    grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a);
-printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b);
-printf("Size of a long init: %lu byte(S)\n", (unsigned long)sizeof(c);
-printf("Size of a long long init: %lu byte(S)\n", (unsigned long)sizeof(d);
+printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
+printf("Size of a long init: %lu byte(S)\n", (unsigned long)sizeof(c));
+printf("Size of a long long init: %lu byte(S)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
 return (0);
 }
-
