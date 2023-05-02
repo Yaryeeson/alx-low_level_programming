736786c5a0088fc9245da309771329ff05b607ba
Yaryeeson <caryeeson@gmail.com>
2023-05-02 16:39:32 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,8 +2,10 @@
 
 /**
 *_putchar - writes character c to stdout
-*@c: char to be printed
+*@c: The char to be printed
+*
 *Return: On success 1
+On error, -1 is returned, and errno is set appropriately
 */
 int _putchar(char c)
 {
